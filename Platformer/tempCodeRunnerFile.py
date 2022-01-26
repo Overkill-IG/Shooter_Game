36bf9066e96226353a3139fd37becffc7a44a932
Varchasv Hoon <75484743+Overkill-IG@users.noreply.github.com>
2022-01-26 19:01:25 +0530

    Add files via upload

---
+++ Platformer/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+                if restart_button.draw(screen):
+                    bg_scroll = 0
+                    world_data = reset_level()
+                    #load in level data and create world
+                    with open(f'level{level}_data.csv', newline='') as csvfile:
+                        reader = csv.reader(csvfile, delimiter=',')
+                        for x, row in enumerate(reader):
+                            for y, tile in enumerate(row):
+                                world_data[x][y] = int(tile)
+                    world = World()
+                    player, health_bar = world.process_data(world_data)
